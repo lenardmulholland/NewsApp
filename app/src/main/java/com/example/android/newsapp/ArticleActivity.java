1de27e4da74ca5a2624e978ab075e418f12b19ad
Jennifer Mulholland <lenardmulholland@gmail.com>
2018-08-25 22:08:08 -0600

    Update: added settings options so user could set preferences on start and end dates for feed and order of articles in the feed.

--- app/src/main/java/com/example/android/newsapp/ArticleActivity.java
+++ app/src/main/java/com/example/android/newsapp/ArticleActivity.java
@@ -4,11 +4,15 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -21,7 +25,7 @@ import java.util.List;
 public class ArticleActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Article>>{
 
     private static final String apiKey = BuildConfig.ApiKey;
-    private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search?q=utah&show-tags=contributor&api-key=" + apiKey;
+    private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search";
     private static final int ARTICLE_LOADER_ID = 1;
     private ArticleAdapter mAdapter;
     private TextView mEmptyStateTextView;
@@ -34,25 +38,29 @@ public class ArticleActivity extends AppCompatActivity implements LoaderManager.
 
         ListView articleListView = findViewById(R.id.list);
 
-        mEmptyStateTextView = findViewById(R.id.empty_view);
-        articleListView.setEmptyView(mEmptyStateTextView);
+        setEmptyState(articleListView);
 
         mAdapter = new ArticleAdapter(this, new ArrayList<Article>());
         articleListView.setAdapter(mAdapter);
 
         progressBar = findViewById(R.id.progress_bar);
 
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        loadArticles();
 
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
+        setArticleOnClickListener(articleListView);
+    }
+
+    private void loadArticles() {
+        if (hasNetwork()) {
             LoaderManager loaderManager = getLoaderManager();
             loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
         } else {
             progressBar.setVisibility(View.GONE);
             mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
+    }
 
+    private void setArticleOnClickListener(ListView articleListView) {
         articleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
@@ -64,9 +72,43 @@ public class ArticleActivity extends AppCompatActivity implements LoaderManager.
         });
     }
 
+    private boolean hasNetwork() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    private void setEmptyState(ListView articleListView) {
+        mEmptyStateTextView = findViewById(R.id.empty_view);
+        articleListView.setEmptyView(mEmptyStateTextView);
+    }
+
     @Override
     public Loader<List<Article>> onCreateLoader(int i, Bundle bundle){
-        return new ArticleLoader(this, GUARDIAN_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String startDate = sharedPrefs.getString(
+                getString(R.string.settings_start_date_key),
+                getString(R.string.settings_start_date_default));
+        String endDate = sharedPrefs.getString(
+                getString(R.string.settings_end_date_key),
+                getString(R.string.settings_end_date_default));
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("q", getString(R.string.search_term));
+        uriBuilder.appendQueryParameter("show-tags", getString(R.string.show_tag_value));
+        uriBuilder.appendQueryParameter("from-date", startDate);
+        uriBuilder.appendQueryParameter("to-date", endDate);
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+        uriBuilder.appendQueryParameter("api-key", apiKey);
+
+        return new ArticleLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -83,5 +125,22 @@ public class ArticleActivity extends AppCompatActivity implements LoaderManager.
     @Override
     public void onLoaderReset(Loader<List<Article>> loader) {
         mAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
